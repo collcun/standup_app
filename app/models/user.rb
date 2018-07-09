851c6f8d8e3bf1617916944ebdb78061e3f0f28c
Collin Cunninghame <collicun@gmail.com>
2018-07-09 16:09:37 -0500

    add user management and invitation functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+  include Friendlyable
+  rolify
   belongs_to :account
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
